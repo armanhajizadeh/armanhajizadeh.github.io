5dadee778613bdc41d5ed076f2aa9fb4a002249c
Arman Hajizadeh Xerda <38994055+armanhajizadeh@users.noreply.github.com>
2023-10-18 10:06:51 +0330

    contactme3

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,25 +1,56 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Include PHPMailer files
+require 'path/to/PHPMailer/Exception.php';
+require 'path/to/PHPMailer/PHPMailer.php';
+require 'path/to/PHPMailer/SMTP.php';
+
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'arman.hajizadeh@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\ n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
-?>
+if (empty($_POST['name']) ||
+    empty($_POST['email']) ||
+    empty($_POST['message']) ||
+    empty($_POST['Phone']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
+    echo "No arguments Provided!";
+    return false;
+}
+
+// Sanitize user inputs
+$name = htmlspecialchars($_POST['name']);
+$email_address = htmlspecialchars($_POST['email']);
+$phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : '';
+$message = htmlspecialchars($_POST['message']);
+
+// Create a new PHPMailer instance
+$mail = new PHPMailer();
+
+// SMTP configuration for Gmail
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'arman.hajizadeh@gmail.com'; // Your Gmail email address
+$mail->Password = 'YM2740851848'; // Your Gmail password
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+// Sender information
+$mail->setFrom('arman.hajizadeh@gmail.com', 'Arman'); // Your Gmail email address and name
+$mail->addAddress('arman.hajizadeh@gmail.com'); // Recipient email address
+$mail->addReplyTo($email_address, $name); // Reply-to email address
+
+// Email content
+$mail->isHTML(false); // Set to true if you want to send HTML email
+$mail->Subject = "Website Contact Form: $name";
+$mail->Body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+
+if ($mail->send()) {
+    echo "Message sent successfully!";
+    return true;
+} else {
+    echo "Message could not be sent. Mailer Error: " . $mail->ErrorInfo;
+    return false;
+}
